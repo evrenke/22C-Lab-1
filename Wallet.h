cf8f91b337ebf74bd61761d41ac1d28e392b66b3
evrenke <evrenkeskin300@gmail.com>
2018-10-02 01:11:39 -0700

    Update Wallet.h

--- Wallet.h
+++ Wallet.h
@@ -1,38 +1,45 @@
 #pragma once
-//#include "Currency.h"
+#include "Currency.h"
 
 class Wallet
 {
 private:
-	//Currency[] c;
+	Currency c[];
 public:
 	enum CurrencyType { DOLLAR, EURO, YEN, RUPEE, YUAN };
-	//Wallet(Currency[] c);
+	Wallet(Currency c[]);
+
+	/*
+	Gives the count of non zero currencies
+	*/
+	int nonZeroCurrencyCount();
 
 	/*
 	Checks if a currency is empty of value
 	*/
-	//bool checkCurrency(CurrencyType cT);
+	bool isEmptyCurrency(CurrencyType cT);
 	
 	/*
 	Adds a currency value to the right object
 	*/
-	//void addCurrency(CurrencyType cT, int whole, int fraction);
+	void addCurrency(CurrencyType cT, int whole, int fraction);
 
 	/*
 	Removes a currency value from the right object
 	THIS HAS TO BE USED ONLY AFTER IT HAS BEEN CONFIRMED THAT THE VALUE PASSED CAN BE REMOVED
 	(so it doesnt reach a negative value)
 	*/
-	//void removeCurrency(CurrencyType cT, int whole, int fraction)
+	void removeCurrency(CurrencyType cT, int whole, int fraction);
 
 	/*
 	This clears all the money from the wallet
 	*/
-	//void emptyWallet()
+	void emptyWallet();
 
 	/*
 	This tells if the wallet is empty of money
 	*/
-	//bool isEmpty();
+	bool isEmpty();
+
+	Currency& operator [](int index);
 };
