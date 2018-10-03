a2ac63a38ad75a924ea28d108064e76cb5342553
evrenke <evrenkeskin300@gmail.com>
2018-10-03 14:44:18 -0700

    Update WalletManager.cpp

--- WalletManager.cpp
+++ WalletManager.cpp
@@ -1,36 +1,45 @@
+/*
+	Authors: Evren Keskin and Jason Hagene
+	Date: 10/03/2018
+	Description:
+		This program showcases the use of polymorhpism, and inheritance
+		by creating a wallet manager that can keep track of different currency values
+		that the user enters into the program
+		Any user interaction is done through WalletManager.cpp
+		and any non-user input related calculations are done in the other files
+*/
+
 #include <iostream>
-//#include <iomanip>
-//#include <string>
 #include "Currency.h"
 #include "Wallet.h"
 
 using namespace std;
 
-void WalletPanel(int cc, Wallet wallet);
+void WalletPanel(int cc, Wallet *wallet);
 void printCurrencies(Wallet w);
 int takeNumberInput(int range);
 double takeValueInput();
-void addMoney(Wallet w);
-void withdrawMoney(Wallet w);
+void addMoney(Wallet *w);
+void withdrawMoney(Wallet *w);
 
 int main()
 {
 	int currencyCount = 0;
 	Currency c[5] = { Dollar() , Euro(), Yen(), Rupee(), Yuan() };
 	Wallet w = Wallet(c);
-	WalletPanel(currencyCount, w);
+	WalletPanel(currencyCount, &w);
 	return 0;
 }
 
-void WalletPanel(int cc, Wallet wallet)
+void WalletPanel(int cc, Wallet *wallet)
 {
 	bool isExiting = false;
 	while (!isExiting)
 	{
-		cout << "    Welcome to Wallet Manager    " << endl;
-		cout << "  You have " << cc << " currencies" << endl;
-		cout << "---------------------------------" << endl;
-		printCurrencies(wallet);
+		cout << "        Welcome to Wallet Manager    " << endl;
+		cout << "          You have " << cc << " currencies" << endl;
+		
+		printCurrencies(*wallet);
 
 		cout << "1) Add money" << endl;
 		cout << "2) Withdraw money" << endl;
@@ -41,20 +50,30 @@ void WalletPanel(int cc, Wallet wallet)
 		{
 		case 1: addMoney(wallet);  break;
 		case 2: withdrawMoney(wallet);  break;
-		case 3: wallet.emptyWallet(); break;
+		case 3: (*wallet).emptyWallet(); break;
 		case 4: isExiting = true; break;
 		}
-		cc = wallet.nonZeroCurrencyCount();
+		cc = (*wallet).nonZeroCurrencyCount();
 	}
 }
 
+/*Function: printCurrencies(Wallet)
+Description:This function prints a table of used currencies
+	and if some currencies are being used it prints a line above and below the table
+	to make for a clearer ui
+
+*/
 void printCurrencies(Wallet w)
 {
+	if (!w.isEmpty())
+		cout << "--------------------------------------------" << endl;
 	for (int ind = 0; ind < 5; ind++)
 	{
-		if (w.isEmptyCurrency(static_cast<Wallet::CurrencyType> (ind)))
+		if (! w.isEmptyCurrency(ind) )
 			cout << w[ind];
 	}
+	if(!w.isEmpty())
+		cout << "--------------------------------------------" << endl;
 }
 
 int takeNumberInput(int range)
@@ -73,7 +92,7 @@ int takeNumberInput(int range)
 		{
 			chosen = -1;
 		}
-	} while (chosen < 0 || chosen > range);
+	} while (chosen <= 0 || chosen > range);
 	system("CLS");
 	return chosen;
 }
@@ -87,7 +106,7 @@ double takeValueInput()
 		string input = "";
 		getline(cin, input);
 		try {
-			chosen = stoi(input);
+			chosen = stod(input);
 		}
 		catch (...)
 		{
@@ -98,7 +117,7 @@ double takeValueInput()
 	return chosen;
 }
 
-void addMoney(Wallet w)
+void addMoney(Wallet *w)
 {
 	cout << "What currency are you adding to your wallet?" << endl;
 	cout << "1) US Dollars " << endl;
@@ -111,26 +130,17 @@ void addMoney(Wallet w)
 	if (input == 6)
 		return;
 
-	cout << "How much " << w[input].getName() << "(s) do you want to add? " << endl;
+	cout << "How much " << (*w)[input - 1].getName() << "(s) do you want to add? " << endl;
 	double addition = takeValueInput();
 	int wholeAddition = (int)addition;
 	int fractionAddition = (int)(addition * 100) % 100;
 
+	(*w).addCurrency(input - 1, wholeAddition, fractionAddition);
 
-	switch (input)
-	{
-	case 1: w.addCurrency(1, wholeAddition, fractionAddition); break;
-	case 2: w.addCurrency(2, wholeAddition, fractionAddition); break;
-	case 3: w.addCurrency(3, wholeAddition, fractionAddition); break;
-	case 4: w.addCurrency(4, wholeAddition, fractionAddition); break;
-	case 5: w.addCurrency(5, wholeAddition, fractionAddition); break;
-		break;
-	}
-	cin.get();
 	system("CLS");
 }
 
-void withdrawMoney(Wallet w)
+void withdrawMoney(Wallet *w)
 {
 	cout << "What currency are you withdrawing from your wallet?" << endl;
 	cout << "1) US Dollars " << endl;
@@ -143,18 +153,16 @@ void withdrawMoney(Wallet w)
 	if (input == 6)
 		return;
 
-	printCurrencies(w);
+	printCurrencies(*w);
 
-	cout << "How much " << w[input].getName() << "(s) do you want to withdraw? " << endl;
-	double addition = takeValueInput();
-	int wholeAddition = (int)addition;
-	int fractionAddition = (int)(addition * 100) % 100;
+	cout << "How much " << (*w)[input - 1].getName() << "(s) do you want to withdraw? " << endl;
+	double withdrawal = takeValueInput();
+	int whole = (int)withdrawal;
+	int fraction = (int)(withdrawal * 100) % 100;
 
 
-	if (w.removeCurrency(input - 1, wholeAddition, fractionAddition))
+	if ((*w).removeCurrency(input - 1, whole, fraction))
 		cout << "Withdrawal complete!" << endl;
 	else
 		cout << "That is too much money to withdraw!" << endl;
-
-	system("CLS");
 }
